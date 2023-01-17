905f6cfc7c637ee81c4a501e13a0a50bf2499d3d
Satoshi Amemiya <amemiya@protonmail.com>
2023-01-18 06:11:11 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,51 @@
+use nix::mount::{mount, MsFlags};
+use nix::sched::{unshare, CloneFlags};
+use nix::unistd::{execvp, fork, getppid, ForkResult};
+use std::process::Command;
+
+use std::ffi::CString;
 fn main() {
-    println!("Hello, world!");
+    match unsafe { fork() } {
+        Ok(ForkResult::Parent { .. }) => {
+            let flags = CloneFlags::CLONE_NEWUSER | CloneFlags::CLONE_NEWNS;
+            unshare(flags).unwrap();
+
+            let flags = MsFlags::MS_BIND;
+            mount(
+                Some("/home/satoshi/tmp/hoge"),
+                "/home/satoshi/tmp/fuga",
+                None::<&str>,
+                flags,
+                None::<&str>,
+            )
+            .unwrap();
+
+            // TODO: wait to create id mapping on child
+            let shell = CString::new("fish").unwrap();
+            execvp(&shell, &[shell.clone()]).unwrap();
+        }
+        Ok(ForkResult::Child) => {
+            let pid = getppid();
+            // TODO: wait to unshare on parrent
+            Command::new("newuidmap")
+                .args([
+                    pid.to_string(),
+                    "0".to_string(),
+                    "1000".to_string(),
+                    "1".to_string()
+                ])
+                .output()
+                .unwrap();
+            Command::new("newgidmap")
+                .args([
+                    pid.to_string(),
+                    "0".to_string(),
+                    "1000".to_string(),
+                    "1".to_string(),
+                ])
+                .output()
+                .unwrap();
+        }
+        Err(_) => println!("Fork failed"),
+    }
 }
